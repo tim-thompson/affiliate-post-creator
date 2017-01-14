b866abc07fe69b07f55f5602e373691d4daff6e7
Tim Thompson <tim.thompson147@gmail.com>
2017-01-14 11:17:55 +0000

    Added key example

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from amazon.api import AmazonAPI
-import requests
-import json
-import sys
+import requests, json, sys
 
 # Load Config
 post_def = sys.argv[1]
@@ -19,7 +17,9 @@ GENIUS_ACCESS_KEY = key_data['genius_access']
 GENIUS_SECRET_KEY = key_data['genius_secret']
 
 # API Setup
+print ('Connecting to Amazon API...')
 amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG, region='UK')
+print ('Connected to Amazon API')
 
 # Load Post Definition
 with open(post_def) as post_file:
@@ -35,26 +35,36 @@ intro = '<' + post_data['title_tag'] + '>' + post_data['intro_title'] + '</' + p
 intro = intro + '<p>' + post_data['intro_copy'] + '</p>'
 products = ''
 
+print ('Looping through products...')
 for product in post_data['products']:
     # Generate genius link
+    print ('Creating geni.us link...')
     payload = {'Url': product['affiliate_link'], 'GroupId': '24142'}
     r = requests.post('https://api.geni.us/v2/shorturl', headers=headers, params=payload)
     link_json = json.loads(r.text)
     link = link_json['NewLink']
+    print ('Created geni.us link')
 
     # Get Amazon Image
+    print ('Getting Amazon image link...')
     amazon_product = amazon.lookup(ItemId=product['asin'])
     image_link = amazon_product.large_image_url
+    print ('Amazon image link retrieved')
 
     products = products + '<a rel="nofollow" target="_blank" href="' + link + '">'
     products = products + '<' + post_data['title_tag'] + '>' + product['title'] + '</' + post_data['title_tag'] + '>'
     products = products + '</a>'
     products = products + '<p><a rel="nofollow" target="_blank" href="' + link + '">'
-    products = products + '<img src="' + image_link + '" alt="' + product['title'] + '" /></a></p>'
+    products = products + '<img class="aligncenter" src="' + image_link + '" alt="' + product['title'] + '" /></a></p>'
     products = products + '<p>' + product['copy'] + '</p>'
     products = products + '<a rel="nofollow" target="_blank" href="' + link + '">' + post_data['buy_button'] + '</a>'
 
 conclusion = '<' + post_data['title_tag'] + '>' + post_data['conclusion_title'] + '</' + post_data['title_tag'] + '>'
 conclusion = conclusion + '<p>' + post_data['conclusion_copy'] + '</p>'
+
+# Write HTML to file
+html = open('post.html', 'w')
+html.write(intro + products + conclusion)
+html.close()
 
 print (intro + products + conclusion)
